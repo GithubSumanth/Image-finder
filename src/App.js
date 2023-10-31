dc9b1ac0b87d3bd872e134ec5669040c60d6d1d0
Sumant Kumbar <my.github.sumanth@gmail.com>
2023-10-31 11:49:22 +0530

    Added the styles for the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import "./App.css";
+import { useState } from "react";
+import ImageList from "./components/ImageList";
+import SearchBar from "./components/SearchBar";
+import searchImages from "./api";
+
 function App() {
-  return <div>App</div>;
+  const [images, setImages] = useState([]);
+  const handleSubmit = async (term) => {
+    const response = await searchImages(term);
+    setImages(response);
+  };
+  return (
+    <div className="page">
+      <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
+    </div>
+  );
 }
 
 export default App;

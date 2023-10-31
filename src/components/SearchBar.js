dc835f6c55661d593fc982c9cc1bfae153cfee71
Sumant Kumbar <my.github.sumanth@gmail.com>
2023-10-31 11:09:29 +0530

    Add code for search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,23 @@
-function SearchBar() {
-  return <div>SearchBar</div>;
+import { useState } from "react";
+
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("");
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(term);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Search Here</label>
+      <input type="text" onChange={handleChange} value={term} />
+    </form>
+  );
 }
 
 export default SearchBar;
